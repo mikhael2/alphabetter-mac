8a61b000bf7e419b3d54fce8056611dee8527001
Mikhael Hayes <156834718+mikhael2@users.noreply.github.com>
2025-12-27 11:42:54 -0500

    More renaming of the project

--- Alphabetter/AlphabetterApp.swift
+++ Alphabetter/AlphabetterApp.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 @main
-struct IPACyclerApp: App {
+struct AlphabetterApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
